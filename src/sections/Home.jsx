35100e7f494188c2903fdf2fd3a0d698d7a926e8
DILKASH ALAM <dilkashalam8101@gmail.com>
2026-07-06 18:33:46 +0530

    Fixed dangerous site issue and cleaned code

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -175,7 +175,7 @@ const Home = React.forwardRef((props, ref) => {
                 View My Work
               </a>
               <a
-                href="/Resume.pdf"
+                href="./Resume/Resume.pdf"
                 download
                 className="px-6 py-3 rounded-full text-lg font-medium text-black bg-white 
                 hover:bg-gray-200 shadow-lg hover:scale-105 transition-all"
